364bf9f1f280a50273423ff20aad219637691b49
adamstory <adamjstory@gmail.com>
2021-12-22 15:33:32 +0000

    Update main.js file with plan - task 1 complete

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-/* 
+/* Plan
+
+Code to include:
+- 
+- Something to do with an array
+- Updating one of the elements within the array
+
+Scenario:
+- List of pokemon, update one of the Pokemon's moves 
+- When the pokemon reaches a certain level, must evolve and change a certain move
+
+*/
+
+/*
 👉 Write your kata here!
+
+You are about to face the Elite Four and your Pokemon has just reached level 65, the next Elite Four member that you are going to face is Wade the Water type leader. You would like to change the move of Dragonite from Fire Blast to Thunder.
+
+let array = [{name: "Cherubi", move: "Charm"}, {name: "Pikachu", move: "Thunderbolt"}, {name: "Dragonite", move: "Fire Blast"}, {name: "Feraligatr", move: "Surf"}]
+
+Return the array with the updated moveset. 
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+function newMove(array){
+    // Insert your solution below
+    
+    return array;
+}
