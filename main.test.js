aa40524312b7d5f3a74a2d913dd92632e6686bbe
Israel <93683580+blueishegg@users.noreply.github.com>
2021-12-22 16:02:13 +0000

    Task 1 - codewars

--- main.test.js
+++ main.test.js
@@ -1,1 +1,26 @@
 //👉 Write your tests below here:
+
+describe("Tests for pokemon kata", function () {
+  test("When array has Dragonite's move property updated to Thunder, test has passed", function () {
+    //arrange
+    const array = [
+      { name: "Cherubi", move: "Charm" },
+      { name: "Pikachu", move: "Thunderbolt" },
+      { name: "Dragonite", move: "Fire Blast" },
+      { name: "Feraligatr", move: "Surf" },
+    ];
+
+    const expected = [
+      { name: "Cherubi", move: "Charm" },
+      { name: "Pikachu", move: "Thunderbolt" },
+      { name: "Dragonite", move: "Thunder" },
+      { name: "Feraligatr", move: "Surf" },
+    ];
+
+    //act
+    const actual = newMove(array);
+
+    //assert
+    expect(actual).toEqual(expected);
+  });
+});
